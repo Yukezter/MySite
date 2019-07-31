5972a9ca3f37480d39586937f5afeddb1ba8d288
Yuke <ray.b.hinchliffe@gmail.com>
2019-07-31 14:34:07 -0700

    added project descriptions

--- client/src/components/pages/Portfolio/ProjectsGrid/projects.js
+++ client/src/components/pages/Portfolio/ProjectsGrid/projects.js
@@ -3,56 +3,56 @@ export default [
     imgSrc: `/images/study-chat.png`,
     href: 'https://github.com/choco-taco/testyoselves',
     title: 'Study chat',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'Study groups don\'t always have to be in person. With Study Chat, you can sign up, create study guides, and invite friends to chat and study with using real time flashcards.',
+    date: '04/28/2019'
   },
   {
     imgSrc: `/images/space-dashboard.png`,
     href: 'https://github.com/choco-taco/space-dashboard',
     title: 'Space Dashboard',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'Love space? Say no more. Visit Space Dashboard for space related information such as current ISS location, Mars Rover photos, asteroids, and much more!',
+    date: '03/19/2019'
   },
   {
     imgSrc: `/images/nfl-statgraph.png`,
     href: 'https://github.com/Yukezter/nfl-statgraph',
     title: 'NFL StatGraph',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'If you\'re into sports and a numbers person, here\'s a script that web scraped ESPN.com for 2018 NFL stats. Team and player stats can be visualized using matplotlib.',
+    date: '09/01/2018'
   },
   {
     imgSrc: `/images/stock-watch.png`,
     href: 'https://github.com/Yukezter/stocker',
-    title: 'StockWatch',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    title: 'Stock Watch',
+    body: 'For any stock enthusients, Stock Watch offers a real time stock data dashboard utilizing sockets and Server Sent Events by IEX Cloud API. Just sign up and add a company to your watchlist.',
+    date: '06/24/2019'
   },
   {
     imgSrc: `/images/liri.png`,
     href: 'https://github.com/Yukezter/liri-node-api',
     title: 'LIRI',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'This is a simple Language Interpretation and Recognition Interface. Search upcoming concerts for any artist or band, details for any song, and information about any movie.',
+    date: '04/18/2019'
   },
   {
     imgSrc: `/images/space-blog2.png`,
     href: 'https://github.com/Yukezter/space-blog',
     title: 'Space Blog',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'A space themed, admin oriented blog site that allows dynamic blog post layouts. Keeping every post unique allows bloggers to better express themselves and keeps readers interested.',
+    date: '11/10/2018'
   },
   {
     imgSrc: `/images/star-wars-rpg.png`,
     href: 'https://github.com/Yukezter/unit-4-game/tree/gh-pages',
     title: 'Star Wars RPG',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'Will you fight as a Jedi or a Sith in this Star Wars RPG? Whichever you choose, make sure you strategize. You just might be one click away from dying in a galaxy far far away...',
+    date: '03/25/2019'
   },
   {
     imgSrc: `/images/trivia.png`,
     href: 'https://github.com/Yukezter/TriviaGame/tree/gh-pages',
     title: 'Trivia Game',
-    body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius autem, aliquam voluptatem laboriosam nihil in recusandae totam facilis sequi provident nobis.',
-    date: '01/01/2020'
+    body: 'Test your trivia skills with this Trivia Game that consists of four categories: Sports, American History, Science, and Animals. Choose wisely, and don\'t run out of time!',
+    date: '05/03/2019'
   },
 ]
